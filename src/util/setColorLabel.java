8638b0e528bf617e3f150ddd07b210415db3c472
khoiduluon <khoiduluon2@gmail.com>
2020-11-04 15:45:58 +0700

    M5: Update 4/11

---
+++ src/util/setColorLabel.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+import java.awt.Color;
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
+import javax.swing.border.BevelBorder;
+
+/**
+ *
+ * @author ldbl
+ */
+public class setColorLabel {
+
+    public void setBorder(JLabel l) {
+//        l.setBackground(new Color(255,0,51));
+        l.setBorder(BorderFactory.createBevelBorder(BevelBorder.LOWERED, new java.awt.Color(162, 10, 10), new java.awt.Color(255, 163, 108)));
+
+    }
+    
+    public void setColor(JLabel l){
+        l.setForeground(new java.awt.Color(255,0,0));
+    }
+    
+    public void resetColor(JLabel l){
+        l.setForeground(new Color(0,102,255));
+    }
+
+    public void reSetBorder(JLabel l) {
+//        l.setBackground(new Color(141, 181, 150));
+        l.setBorder(BorderFactory.createEmptyBorder());
+
+    }
+}
