3f33ee16adec29246aceb5c269563bc4e0ebf900
NganMapTran <doanhuynhlongngan161200@gmail.com>
2020-11-09 16:29:38 +0700

    Ngan

--- src/com/entity/MucTieu.java
+++ src/com/entity/MucTieu.java
@@ -10,5 +10,91 @@ package com.entity;
  * @author trantrung
  */
 public class MucTieu {
-    String tenMT;
+    private String tenMucTieu;
+    private double giaTri;
+    private double soTienDaTK;
+    private int thoiHan;
+    private String ngayTao;
+
+    public MucTieu() {
+    }
+
+    public MucTieu(String tenMucTieu, double giaTri, double soTienDaTK, int thoiHan, String ngayTao) {
+        this.tenMucTieu = tenMucTieu;
+        this.giaTri = giaTri;
+        this.soTienDaTK = soTienDaTK;
+        this.thoiHan = thoiHan;
+        this.ngayTao = ngayTao;
+    }
+
+    /**
+     * @return the tenMucTieu
+     */
+    public String getTenMucTieu() {
+        return tenMucTieu;
+    }
+
+    /**
+     * @param tenMucTieu the tenMucTieu to set
+     */
+    public void setTenMucTieu(String tenMucTieu) {
+        this.tenMucTieu = tenMucTieu;
+    }
+
+    /**
+     * @return the giaTri
+     */
+    public double getGiaTri() {
+        return giaTri;
+    }
+
+    /**
+     * @param giaTri the giaTri to set
+     */
+    public void setGiaTri(double giaTri) {
+        this.giaTri = giaTri;
+    }
+
+    /**
+     * @return the soTienDaTK
+     */
+    public double getSoTienDaTK() {
+        return soTienDaTK;
+    }
+
+    /**
+     * @param soTienDaTK the soTienDaTK to set
+     */
+    public void setSoTienDaTK(double soTienDaTK) {
+        this.soTienDaTK = soTienDaTK;
+    }
+
+    /**
+     * @return the thoiHan
+     */
+    public int getThoiHan() {
+        return thoiHan;
+    }
+
+    /**
+     * @param thoiHan the thoiHan to set
+     */
+    public void setThoiHan(int thoiHan) {
+        this.thoiHan = thoiHan;
+    }
+
+    /**
+     * @return the ngayTao
+     */
+    public String getNgayTao() {
+        return ngayTao;
+    }
+
+    /**
+     * @param ngayTao the ngayTao to set
+     */
+    public void setNgayTao(String ngayTao) {
+        this.ngayTao = ngayTao;
+    }
+    
 }
---
+++ src/util/SQL.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+/**
+ *
+ * @author Rom
+ */
+public class SQL {
+    /*
+    create database QLTK
+go
+use QLTk
+go
+create table NguoiDung(
+	Username nvarchar(50) primary key,
+	TenND nvarchar(50) not null,
+	GioiTinh bit,
+	Pass nvarchar(50) not null,
+	SoDu float not null
+)
+
+create table MucTieuTietKiem(
+	IDMucTieu int identity(1,1) primary key,
+	Username nvarchar(50) not null,
+	TenMT nvarchar(100) not null,
+	GiaTri float not null,
+	ThoiHan int not null,
+	SoTienDaTK float null,
+	NgayDK date Default(Getdate())
+)
+
+create table ThongKeTienDo(
+	IDMucTieu int primary key,
+	TienDo int not null,
+	ThoiGianConLai int not null
+)
+
+create table LichSu(
+	IDLichSu int identity(1,1) primary key,
+	IDMucTieu int not null,
+	NgayTK date not null,
+	SoTienTK float not null
+)
+
+create table QuanLyThu(
+	IDThu int identity(1,1) primary key,
+	Username nvarchar(50) not null,
+	LoaiGD nvarchar(100) not null,
+	SoTien float not null,
+	NgayGD date not null
+)
+
+create table QuanLyChi(
+	IDChi int identity(1,1) primary key,
+	Username nvarchar(50) not null,
+	LoaiGD nvarchar(100) not null,
+	SoTien float not null,
+	NgayGD date not null
+)
+
+create table ThongKeChiTieu(
+	Username nvarchar(50) primary key,
+	TongThu float not null,
+	TongChi float not null,
+	SoDu float not null
+)
+
+alter table MucTieuTietKiem
+add constraint FK_MTTK_ND
+foreign key (Username)
+references NguoiDung(Username)
+
+alter table LichSu
+add constraint FK_LS_MTTK
+foreign key (IDMucTieu)
+references MucTieuTietKiem(IDMucTieu)
+
+alter table MucTieuTietKiem
+add constraint FK_MTTK_TKTD
+foreign key (IDMucTieu)
+references ThongKeTienDo(IDMucTieu)
+
+alter table QuanLyThu
+add constraint FK_QLT_ND
+foreign key (Username)
+references NguoiDung(Username)
+
+alter table QuanLyChi
+add constraint FK_QLC_ND
+foreign key (Username)
+references NguoiDung(Username)
+
+alter table ThongKeChiTieu
+add constraint FK_TKCT_ND
+foreign key (Username)
+references NguoiDung(Username)
+    */
+}
